52a8ab71847717c5722a47f9515420de1a594b20
Viliam Kopecky <vk@viliamkopecky.cz>
2019-09-01 10:04:51 +0200

    Prefer home directory installs

--- src/config.ts
+++ src/config.ts
@@ -4,8 +4,31 @@ import path from 'path'
 import pkg from '../package.json'
 
 const installedDirname = 'installed'
-const installedDirPath = path.join(os.tmpdir(), 'npxm', pkg.name, pkg.version, installedDirname)
-if (!fs.existsSync(installedDirPath)) {
-	fs.mkdirpSync(installedDirPath)
+
+const tryPaths = [
+	path.join(os.homedir(), '.npxm', pkg.name, pkg.version, installedDirname),
+	path.join(os.tmpdir(), 'npxm', pkg.name, pkg.version, installedDirname),
+]
+
+export let installedDir: string = ''
+
+for (const installedDirPath of tryPaths) {
+	if (!fs.existsSync(installedDirPath)) {
+		try {
+			fs.mkdirpSync(installedDirPath)
+			installedDir = installedDirPath
+			break
+		} catch (e) {
+			console.trace(e)
+		}
+	} else {
+		installedDir = installedDirPath
+		break
+	}
 }
-export const installedDir = fs.realpathSync(installedDirPath)
+
+if (!installedDir) {
+	throw new Error('Cannot create cache dir')
+} else {
+	console.log(installedDir)
+}
